49e5b3b69fc55f95bdbd66d4ae4c5e66c869131a
Javier Gamarra <javier.gamarra@liferay.com>
2015-06-23 09:21:05 +0200

    LSR-98 base interactor for audience targeting

---
+++ android/library/core/src/main/java/com/liferay/mobile/screens/audiencetargeting/interactor/AudienceTargetingInteractor.java
@@ -0,0 +1,10 @@
+package com.liferay.mobile.screens.audiencetargeting.interactor;
+
+import com.liferay.mobile.screens.audiencetargeting.AudienceTargetingListener;
+import com.liferay.mobile.screens.base.interactor.Interactor;
+
+/**
+ * @author Javier Gamarra
+ */
+public interface AudienceTargetingInteractor extends Interactor<AudienceTargetingListener> {
+}
