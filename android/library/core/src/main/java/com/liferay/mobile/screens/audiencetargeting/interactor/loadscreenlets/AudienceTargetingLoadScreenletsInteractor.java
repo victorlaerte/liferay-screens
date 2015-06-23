3e86f722da300ec58e94e14a3b7394d9f175f284
Javier Gamarra <javier.gamarra@liferay.com>
2015-06-23 09:20:36 +0200

    LSR-98 load interactor for audience

---
+++ android/library/core/src/main/java/com/liferay/mobile/screens/audiencetargeting/interactor/loadscreenlets/AudienceTargetingLoadScreenletsInteractor.java
@@ -0,0 +1,18 @@
+package com.liferay.mobile.screens.audiencetargeting.interactor.loadscreenlets;
+
+import com.liferay.mobile.screens.audiencetargeting.AudienceTargetingListener;
+import com.liferay.mobile.screens.audiencetargeting.UserContext;
+import com.liferay.mobile.screens.audiencetargeting.interactor.AudienceTargetingInteractor;
+import com.liferay.mobile.screens.base.interactor.Interactor;
+
+/**
+ * @author Javier Gamarra
+ */
+public interface AudienceTargetingLoadScreenletsInteractor extends AudienceTargetingInteractor {
+
+	void getScreenlets(String screenletApp, Integer groupId, UserContext userContext) throws Exception;
+
+	void getScreenlets(String screenletApp, Integer groupId, String placeholder, UserContext userContext)
+		throws Exception;
+
+}
---
+++ android/library/core/src/main/java/com/liferay/mobile/screens/audiencetargeting/interactor/loadscreenlets/AudienceTargetingLoadScreenletsInteractorImpl.java
@@ -0,0 +1,65 @@
+package com.liferay.mobile.screens.audiencetargeting.interactor.loadscreenlets;
+
+import com.liferay.mobile.android.service.Session;
+import com.liferay.mobile.android.v62.dlfileentry.DLFileEntryService;
+import com.liferay.mobile.screens.audiencetargeting.AudienceTargetingListener;
+import com.liferay.mobile.screens.audiencetargeting.UserContext;
+import com.liferay.mobile.screens.audiencetargeting.interactor.AudienceTargetingCallback;
+import com.liferay.mobile.screens.audiencetargeting.interactor.AudienceTargetingResult;
+import com.liferay.mobile.screens.audiencetargeting.interactor.requestcontent.AudienceTargetingContentEvent;
+import com.liferay.mobile.screens.base.interactor.BaseRemoteInteractor;
+import com.liferay.mobile.screens.base.interactor.JSONObjectCallback;
+import com.liferay.mobile.screens.base.interactor.JSONObjectEvent;
+import com.liferay.mobile.screens.context.SessionContext;
+import com.liferay.mobile.screens.service.v62.ScreensAudienceTargetingService;
+import com.liferay.mobile.screens.util.LiferayLogger;
+
+import org.json.JSONObject;
+
+/**
+ * @author Javier Gamarra
+ */
+public class AudienceTargetingLoadScreenletsInteractorImpl
+	extends BaseRemoteInteractor<AudienceTargetingListener>
+	implements AudienceTargetingLoadScreenletsInteractor {
+
+	public AudienceTargetingLoadScreenletsInteractorImpl(int targetScreenletId) {
+		super(targetScreenletId);
+	}
+
+	public void getScreenlets(String screenletApp, Integer groupId, UserContext userContext)
+		throws Exception {
+
+		getScreenlets(screenletApp, groupId, null, userContext);
+	}
+
+	public void getScreenlets(String screenletApp, Integer groupId, String placeholder, UserContext userContext)
+		throws Exception {
+
+		JSONObject jsonObject = userContext.toJSON();
+
+		ScreensAudienceTargetingService service = getAudienceTargetingService();
+		service.getContent(screenletApp, groupId, placeholder, jsonObject, null);
+	}
+
+	public void onEvent(AudienceTargetingLoadedEvent event) {
+		if (!isValidEvent(event)) {
+			return;
+		}
+
+		if (event.isFailed()) {
+			getListener().onFailure(event.getException());
+		}
+		else {
+			getListener().onSuccess(event);
+		}
+	}
+
+	private ScreensAudienceTargetingService getAudienceTargetingService() {
+		Session session = SessionContext.createSessionFromCurrentSession();
+		session.setCallback(new AudienceTargetingCallback(getTargetScreenletId()));
+		return new ScreensAudienceTargetingService(session);
+	}
+
+}
+
