74e63a8dd61e488228e3d97da6f9b96858bd7787
Javier Gamarra <javier.gamarra@liferay.com>
2015-06-23 09:24:50 +0200

    LSR-98 user context for interactor calls

---
+++ android/library/core/src/main/java/com/liferay/mobile/screens/audiencetargeting/UserContext.java
@@ -0,0 +1,28 @@
+package com.liferay.mobile.screens.audiencetargeting;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * @author Javier Gamarra
+ */
+public class UserContext {
+
+	private Long userId;
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(final Long userId) {
+		this.userId = userId;
+	}
+
+	public JSONObject toJSON() throws JSONException {
+		JSONObject jsonObject = new JSONObject();
+
+		jsonObject.put("userId", userId);
+
+		return jsonObject;
+	}
+}
