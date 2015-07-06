15315c18f598c868027edcdb015bc93480d45b0c
Javier Gamarra <javier.gamarra@liferay.com>
2015-07-06 15:43:48 +0200

    LSR-98 new API services

--- android/library/core/src/main/java/com/liferay/mobile/screens/service/v62/ScreensAudienceTargetingService.java
+++ android/library/core/src/main/java/com/liferay/mobile/screens/service/v62/ScreensAudienceTargetingService.java
@@ -31,16 +31,17 @@ public class ScreensAudienceTargetingService extends BaseService {
 		super(session);
 	}
 
-	public JSONObject addScreensMobile(String appId, String placeholderId, long assetEntryId, String customContentText, JSONObjectWrapper serviceContext) throws Exception {
+	public JSONObject addScreensMobile(long tacticId, String appId, String placeholderId, long assetEntryId, JSONObject customContentMap, JSONObjectWrapper serviceContext) throws Exception {
 		JSONObject _command = new JSONObject();
 
 		try {
 			JSONObject _params = new JSONObject();
 
+			_params.put("tacticId", tacticId);
 			_params.put("appId", checkNull(appId));
 			_params.put("placeholderId", checkNull(placeholderId));
 			_params.put("assetEntryId", assetEntryId);
-			_params.put("customContentText", checkNull(customContentText));
+			_params.put("customContentMap", checkNull(customContentMap));
 			mangleWrapper(_params, "serviceContext", "com.liferay.portal.service.ServiceContext", serviceContext);
 
 			_command.put("/channel-screens-mobile.screensmobile/add-screens-mobile", _params);
@@ -111,6 +112,33 @@ public class ScreensAudienceTargetingService extends BaseService {
 		return _result.getJSONArray(0);
 	}
 
+	public JSONArray getContent(String appId, long groupId, JSONArray placeholderIds, JSONObject userContext, JSONObjectWrapper serviceContext) throws Exception {
+		JSONObject _command = new JSONObject();
+
+		try {
+			JSONObject _params = new JSONObject();
+
+			_params.put("appId", checkNull(appId));
+			_params.put("groupId", groupId);
+			_params.put("placeholderIds", checkNull(placeholderIds));
+			_params.put("userContext", checkNull(userContext));
+			mangleWrapper(_params, "serviceContext", "com.liferay.portal.service.ServiceContext", serviceContext);
+
+			_command.put("/channel-screens-mobile.screensmobile/get-content", _params);
+		}
+		catch (JSONException _je) {
+			throw new Exception(_je);
+		}
+
+		JSONArray _result = session.invoke(_command);
+
+		if (_result == null) {
+			return null;
+		}
+
+		return _result.getJSONArray(0);
+	}
+
 	public JSONObject getScreensMobile(long screensMobileId) throws Exception {
 		JSONObject _command = new JSONObject();
 
@@ -134,31 +162,7 @@ public class ScreensAudienceTargetingService extends BaseService {
 		return _result.getJSONObject(0);
 	}
 
-	public JSONObject updateScreensMobile(long screensMobileId, long tacticId) throws Exception {
-		JSONObject _command = new JSONObject();
-
-		try {
-			JSONObject _params = new JSONObject();
-
-			_params.put("screensMobileId", screensMobileId);
-			_params.put("tacticId", tacticId);
-
-			_command.put("/channel-screens-mobile.screensmobile/update-screens-mobile", _params);
-		}
-		catch (JSONException _je) {
-			throw new Exception(_je);
-		}
-
-		JSONArray _result = session.invoke(_command);
-
-		if (_result == null) {
-			return null;
-		}
-
-		return _result.getJSONObject(0);
-	}
-
-	public JSONObject updateScreensMobile(long screensMobileId, String appId, String placeholderId, long assetEntryId, String customContentText, JSONObjectWrapper serviceContext) throws Exception {
+	public JSONObject updateScreensMobile(long screensMobileId, String appId, String placeholderId, long assetEntryId, JSONObject customContentMap, JSONObjectWrapper serviceContext) throws Exception {
 		JSONObject _command = new JSONObject();
 
 		try {
@@ -168,7 +172,7 @@ public class ScreensAudienceTargetingService extends BaseService {
 			_params.put("appId", checkNull(appId));
 			_params.put("placeholderId", checkNull(placeholderId));
 			_params.put("assetEntryId", assetEntryId);
-			_params.put("customContentText", checkNull(customContentText));
+			_params.put("customContentMap", checkNull(customContentMap));
 			mangleWrapper(_params, "serviceContext", "com.liferay.portal.service.ServiceContext", serviceContext);
 
 			_command.put("/channel-screens-mobile.screensmobile/update-screens-mobile", _params);
