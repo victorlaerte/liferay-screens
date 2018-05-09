121f555a1f6ece9ff1a455e0c309ec46b90eac4b
Victor Oliveira <victorlaertedoliveira@gmail.com>
2018-05-09 22:45:27 +0200

    Add new fields in mock, and render.

--- android/library/src/main/java/com/liferay/mobile/screens/thingscreenlet/delegates/ConverterDelegate.kt
+++ android/library/src/main/java/com/liferay/mobile/screens/thingscreenlet/delegates/ConverterDelegate.kt
@@ -14,81 +14,21 @@
 
 package com.liferay.mobile.screens.thingscreenlet.delegates
 
-import com.liferay.mobile.screens.ddl.model.DDMStructure
-import com.liferay.mobile.screens.ddl.model.Field
-import com.liferay.mobile.screens.ddl.model.StringField
 import com.liferay.mobile.screens.ddm.form.model.FormInstance
 import com.liferay.mobile.screens.thingscreenlet.model.BlogPosting
 import com.liferay.mobile.screens.thingscreenlet.model.Collection
-import com.liferay.mobile.screens.thingscreenlet.model.Pages
 import com.liferay.mobile.screens.thingscreenlet.model.Person
 import com.liferay.mobile.sdk.apio.delegates.converters
-import com.liferay.mobile.sdk.apio.extensions.asDate
-import com.liferay.mobile.sdk.apio.graph
-import com.liferay.mobile.sdk.apio.model.Relation
-import com.liferay.mobile.sdk.apio.model.Thing
-import com.liferay.mobile.sdk.apio.model.get
-import org.json.JSONArray
-import org.json.JSONObject
-import java.util.*
 
 class ConverterDelegate {
 
     companion object {
         @JvmStatic
         fun initializeConverter() {
-
-            converters[FormInstance::class.java.name] = { it: Thing ->
-
-                val name = it["name"] as String
-                val description = it["description"] as String
-
-                val structureRelation = it.attributes["structure"] as Relation
-
-                val structureDescription = structureRelation["description"] as String
-                val structureName = structureRelation["name"] as String
-
-                val attributes = mapOf(
-                    "dataType" to Field.DataType.STRING.value,
-                    "readOnly" to false,
-                    "type" to Field.EditorType.TEXT.value,
-                    "required" to false,
-                    "showLabel" to true,
-                    "repeatable" to false,
-                    "label" to "TextField single",
-                    "name" to "TextFieldSingle",
-                    "tip" to "TextField hint",
-                    "placeHolder" to ""
-                    )
-
-                val stringField = StringField(attributes, Locale.ENGLISH, Locale.ENGLISH)
-
-                val attributes2 = mapOf(
-                    "dataType" to Field.DataType.STRING.value,
-                    "readOnly" to false,
-                    "type" to Field.EditorType.TEXT_AREA.value,
-                    "required" to false,
-                    "showLabel" to true,
-                    "repeatable" to false,
-                    "label" to "TextField multiple",
-                    "name" to "TextFieldMultiple",
-                    "tip" to "TextField multiple hint",
-                    "placeHolder" to ""
-                )
-
-                val stringField2 = StringField(attributes2, Locale.ENGLISH, Locale.ENGLISH)
-
-                val fields = ArrayList<Field<*>>()
-                fields.add(stringField)
-                fields.add(stringField2)
-
-                val ddmStructure = DDMStructure(structureName, structureDescription, fields)
-
-                FormInstance(name, description, ddmStructure)
-            }
             converters[BlogPosting::class.java.name] = BlogPosting.converter
             converters[Collection::class.java.name] = Collection.converter
             converters[Person::class.java.name] = Person.converter
+            converters[FormInstance::class.java.name] = FormInstance.converter
         }
     }
 }
