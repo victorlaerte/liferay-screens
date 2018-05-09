40061723077459e38c275eea9ab66ebb3f931578
Sarai Díaz García <dgarciasarai@gmail.com>
2018-05-09 15:53:02 +0200

    First version of DDMForm converter

--- android/library/src/main/java/com/liferay/mobile/screens/thingscreenlet/delegates/ConverterDelegate.kt
+++ android/library/src/main/java/com/liferay/mobile/screens/thingscreenlet/delegates/ConverterDelegate.kt
@@ -14,6 +14,10 @@
 
 package com.liferay.mobile.screens.thingscreenlet.delegates
 
+import com.liferay.mobile.screens.ddl.model.DDMStructure
+import com.liferay.mobile.screens.ddl.model.Field
+import com.liferay.mobile.screens.ddl.model.StringField
+import com.liferay.mobile.screens.ddm.form.model.FormInstance
 import com.liferay.mobile.screens.thingscreenlet.model.BlogPosting
 import com.liferay.mobile.screens.thingscreenlet.model.Collection
 import com.liferay.mobile.screens.thingscreenlet.model.Pages
@@ -24,6 +28,8 @@ import com.liferay.mobile.sdk.apio.graph
 import com.liferay.mobile.sdk.apio.model.Relation
 import com.liferay.mobile.sdk.apio.model.Thing
 import com.liferay.mobile.sdk.apio.model.get
+import org.json.JSONObject
+import java.util.*
 
 class ConverterDelegate {
 
@@ -32,8 +38,11 @@ class ConverterDelegate {
         fun initializeConverter() {
             converters[BlogPosting::class.java.name] = { it: Thing ->
                 BlogPosting(
-                    it["headline"] as? String, it["alternativeHeadline"] as? String, it["articleBody"] as? String,
-                    it["creator"] as? Relation, (it["dateCreated"] as? String)?.asDate())
+                    it["headline"] as? String,
+                    it["alternativeHeadline"] as? String,
+                    it["articleBody"] as? String,
+                    it["creator"] as? Relation,
+                    (it["dateCreated"] as? String)?.asDate())
             }
 
             converters[Collection::class.java.name] = { it: Thing ->
@@ -62,6 +71,52 @@ class ConverterDelegate {
                 val image = it["image"] as? String
 
                 Person(name, email, jobTitle, birthDate, image)
+            }
+
+            converters[FormInstance::class.java.name] = { it: Thing ->
+                val formInstanceId = it.id.toLong()
+
+                val attributes = HashMap<String, Any>()
+                attributes["dataType"] = Field.DataType.STRING
+                attributes["readOnly"] = false
+                attributes["type"] = Field.EditorType.TEXT
+                attributes["required"] = false
+                attributes["showLabel"] = true
+                attributes["repeatable"] = false
+                attributes["label"] = "TextField single"
+                attributes["name"] = "TextFieldSingle"
+                attributes["tip"] = "TextField hint"
+                attributes["placeHolder"] = ""
+                val stringField = StringField(attributes, Locale.ENGLISH, Locale.ENGLISH)
+
+                val attributes2 = HashMap<String, Any>()
+                attributes["dataType"] = Field.DataType.STRING
+                attributes["readOnly"] = false
+                attributes["type"] = Field.EditorType.TEXT_AREA
+                attributes["required"] = false
+                attributes["showLabel"] = true
+                attributes["repeatable"] = false
+                attributes["label"] = "TextField multiple"
+                attributes["name"] = "TextFieldMultiple"
+                attributes["tip"] = "TextField multiple hint"
+                attributes["placeHolder"] = ""
+                val stringField2 = StringField(attributes2, Locale.ENGLISH, Locale.ENGLISH)
+
+                val fields = ArrayList<Field<*>>()
+                fields.add(stringField)
+                fields.add(stringField2)
+
+                val ddmStructure = DDMStructure()
+
+                val jsonObject = JSONObject()
+                jsonObject.put("descriptionCurrentValue", "Basic fields form")
+                jsonObject.put("structureId", "36579")
+                jsonObject.put("nameCurrentValue", "Basic fields")
+
+                ddmStructure.parse(jsonObject)
+                ddmStructure.fields = fields
+
+                FormInstance(formInstanceId, ddmStructure);
             }
         }
     }
